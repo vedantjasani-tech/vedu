6cf36a7b1a3fcd41de2c58f01a0aea1c9eea88ba
vedantjasani-tech <55083107+vedantjasani-tech@users.noreply.github.com>
2019-09-09 15:16:20 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+
+import time
+
+
+options = webdriver.ChromeOptions()
+options.headless = True
+browser = webdriver.Chrome(chrome_options = options)
+elem = browser.find_element_by_link_text('Login')
+elem.click()
+time.sleep(7)
+em = browser.find_element_by_id('email')
+em.send_keys('hello@atg.world')
+time.sleep(5)
+pas = browser.find_element_by_id('password')
+pas.send_keys('Pass@123')
+time.sleep(5)
+browser.get('http://www.atg.party/article/')
+time.sleep(5)
+titl = browser.find_element_by_id('title')
+titl.send_keys('test by VEDANT JASANI')
+time.sleep(5)
+desc = browser.find_element_by_class_name('fr-element.fr-view')
+desc.send_keys('Description of VEDANT JASANI created by aws!!!')
+time.sleep(5)
+pos = browser.find_element_by_id('featurebutton')
+
+print("Output sent")
